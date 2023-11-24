3f40e33d7125ec471b845f3264a7d42fe1c6e2e3
Suryakant Thombre <suryathombre000@gmail.com>
2023-11-24 23:28:06 +0530

    navigation, daisyui added

--- Components/menuSlider/MenuSlider.js
+++ Components/menuSlider/MenuSlider.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Link from "next/link";
 
 const MenuSlider = () => {
-  return <div>MenuSlider</div>;
+  return (
+    <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+      <li>
+        <Link href="/about-me">About Me</Link>
+      </li>
+      <li>
+        <Link href="/experience">Experience</Link>
+      </li>
+      <li>
+        <Link href="/resume">Resume</Link>
+      </li>
+      <li>
+        <Link href="/machine_coding">React Machine Coding</Link>
+      </li>
+      <li>
+        <Link href="/projects">Projects</Link>
+      </li>
+      <li>
+        <Link href="/contact-me">Contact Me</Link>
+      </li>
+    </ul>
+  );
 };
 
 export default MenuSlider;
